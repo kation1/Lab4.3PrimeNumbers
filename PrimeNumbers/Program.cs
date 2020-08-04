a1855d23d79b68e0a665d895542cb49d42cab63b
Kathryn Pidek <KathrynPidek@quickenloans.com>
2020-08-04 08:47:11 -0400

    missing goodbye statement

--- PrimeNumbers/Program.cs
+++ PrimeNumbers/Program.cs
@@ -14,9 +14,11 @@ namespace PrimeNumbers
                 int userInput = int.Parse(Console.ReadLine());
                 Console.WriteLine($"\nThe number {userInput} prime is {PrimeNum.GetPrime(userInput)}.");
 
-                Console.WriteLine($"\nDo you wantto find another prime? (y/n)");
+                Console.WriteLine($"\nDo you want to find another prime? (y/n)");
                 running = Console.ReadLine();
             } while (running != "n");
+
+            Console.WriteLine("Thank you! Good Bye!");
         }
     }
 }
